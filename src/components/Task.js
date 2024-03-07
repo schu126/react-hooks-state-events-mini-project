09db42f89949bf31490dde5fca0aa7bb82cebbfd
Samuel Chu <samueljchu0126@gmail.com>
2024-03-07 05:50:30 -0500

    completed states and events lab

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-function Task() {
+// don't miss the {} inside the function
+
+function Task({ task, category, text, deleteThis}) { // make sure to have the 3 argument tasks, category, text (even if you're only using 1 or 2 of the 3)
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button onClick={() => deleteThis(task)} className="delete">X</button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+// don't miss the {} inside the function
+
+function TaskList({ tasks, category, text, deleteThis }) { // make sure to have the 3 argument tasks, category, text (even if you're only using 1 or 2 of the 3)
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {tasks.map(task => (
+        <Task key={task.text} task={task} category={task.category} text={task.text} deleteThis={deleteThis}></Task>
+      ))}
     </div>
   );
 }
